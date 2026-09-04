efd74bdf80160d65727e29b7b5894dbb928b1caf
boitumelojsekonyane-dev <boitumelojsekonyane@gmail.com>
2026-09-04 20:08:52 +0200

    added the events table

--- SQL Script.sql
+++ SQL Script.sql
@@ -33,3 +33,27 @@ CREATE TABLE EventTypes (
     Description VARCHAR(150) NULL,
     IsActive BIT NOT NULL DEFAULT 1
 );
+
+CREATE TABLE Events (
+    EventID INT IDENTITY(1,1) PRIMARY KEY,
+    AdminID INT NOT NULL,
+    EventTypeID INT NOT NULL,
+    Title VARCHAR(150) NOT NULL,
+    Description VARCHAR(500) NOT NULL,
+    EventDate DATE NOT NULL,
+    StartTime TIME NOT NULL,
+    Location VARCHAR(150) NOT NULL,
+    DistanceKm DECIMAL(6,2) NOT NULL,
+    RegistrationOpens DATE NOT NULL,
+    RegistrationCloses DATE NOT NULL,
+    EntryFee DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+    StatusOfEvent VARCHAR(30) NOT NULL DEFAULT 'UPCOMING!',
+
+    CONSTRAINT FK_Events_Admins
+        FOREIGN KEY (AdminID)
+        REFERENCES Admins(AdminID),
+
+    CONSTRAINT FK_Events_EventTypes
+        FOREIGN KEY (EventTypeID)
+        REFERENCES EventTypes(EventTypeID)
+);
